7698bfc27c54bf5af3fa965691a1c0714adcbae2
Ryan Bartlett <ryanzbartlett@gmail.com>
2025-05-14 20:00:42 -0400

    update account view

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
 import LayoutDefault from '@/layouts/LayoutDefault.vue';
+import { useUserStore } from '@/stores/userStore';
+
+const userStore = useUserStore();
 </script>
 
 <template>
-    <LayoutDefault class="account-view">
+    <LayoutDefault class="account-view flex flex-col gap-4">
         <h2>My Account</h2>
+        <form>
+            <div class="flex flex-col gap-2">
+                <div class="form-group">
+                    <label for="user-name">Name</label>
+                    <input id="user-name" type="text" :value="userStore.user?.name" readonly />
+                </div>
+                <div class="form-group">
+                    <label for="user-email">Email</label>
+                    <input id="user-email" type="email" :value="userStore.user?.email" readonly />
+                </div>
+            </div>
+        </form>
     </LayoutDefault>
 </template>

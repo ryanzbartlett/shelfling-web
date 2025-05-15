7698c239fe230fa4d563faaa7c4255b254b4b8b0
Ryan Bartlett <ryanzbartlett@gmail.com>
2025-05-14 21:34:34 -0400

    invalidate queries when access is revoked

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,9 +1,11 @@
 import { shelflingApi } from '@/api/shelflingApi';
+import { useQueryClient } from '@tanstack/vue-query';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useUserStore = defineStore('user', () => {
     const user = ref<ApiService.User>();
+    const queryClient = useQueryClient();
 
     function setUser(newUser: ApiService.User) {
         user.value = newUser;
@@ -13,7 +15,10 @@ export const useUserStore = defineStore('user', () => {
         user.value = undefined;
     }
 
-    shelflingApi.onAccessRevoked(clearUser);
+    shelflingApi.onAccessRevoked(() => {
+        clearUser();
+        queryClient.invalidateQueries();
+    });
 
     return {
         user,
